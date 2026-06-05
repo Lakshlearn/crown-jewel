dddd303fe56b478fd1f952cfb483e8a5f3e7b1e5
Vivek Dahiya <vivekdahiya312@gmail.com>
2026-06-05 14:38:21 +0530

    Update gallery.php to add new images to the gallery

--- pages/gallery.php
+++ pages/gallery.php
@@ -44,7 +44,14 @@
             // Use a root-relative path so the image resolves correctly
             // regardless of how this page is served/embedded.
             $galleryImages = [
-                "../images/gallery/red_carnelian.png",
+                '../images/gallery/red_carnelian.png',
+                '../images/gallery/black_rutile.png',
+                '../images/gallery/green_carnelian.png',
+                '../images/gallery/hermatite.png',
+                '../images/gallery/rainbow_flourite.png',
+                '../images/gallery/red_carnelian.png',
+                '../images/gallery/sodalite.png',
+                '../images/gallery/tiger_eye_red.png',
             ];
 
             foreach ($galleryImages as $image):
