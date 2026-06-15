bdfe5706179b8fc97d8146287081ce21ba37b216
Lakshlearn <deskloventures@gmail.com>
2026-06-15 15:30:40 +0530

    removed 's' from crown jewel

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
     <div class="logo">
-        CROWN JEWELS
+        CROWN JEWEL
     </div>
 
     <nav class="nav">
