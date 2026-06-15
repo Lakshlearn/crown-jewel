c612343c7022e5438b391b815577023688bd3bcc
Lakshlearn <deskloventures@gmail.com>
2026-06-15 02:51:00 -0700

    Update page title from "Crown Web" to "Crown Jewels" for branding consistency.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crown Web</title>
+    <title>Crown Jewels</title>
     <link rel="icon" type="image/png" sizes="60x60" href="images/favicon.png">
     <link rel="stylesheet" href='css/style.css'>
 </head>
